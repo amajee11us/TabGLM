124c4b6581471e0673b5f5b70834dedbde181ddc
Anay Majee <anaymajee11us@gmail.com>
2024-07-09 12:45:50 -0700

    Bug Fix for TAPEX model

--- tgrl/models/text_model.py
+++ tgrl/models/text_model.py
@@ -17,15 +17,22 @@ class TextNetwork(nn.Module):
                 "Text Model of type : {}, does not exist.".format(encoder_name)
             )
 
+        # TODO : For TAPEX just use the encoder and do not load the parameters of decoder
+        self.encoder_type = encoder_name
         self.model = model_dict[encoder_name]["model"]
         self.tokenizer = model_dict[encoder_name]["tokenizer"]
         self.embedding_dim = model_dict[encoder_name]["embedding_dim"]
 
         self.__init__parameters()
-    
+        print(self.model)
+
     def __init__parameters(self):
-        for param in self.model.encoder.parameters():
-            param.requires_grad = False
+        if self.encoder_type == "tapex":  # TAPEX has both encoder and decoder
+            for param in self.model.parameters():
+                param.requires_grad = False
+        else:
+            for param in self.model.encoder.parameters():
+                param.requires_grad = False
 
     def forward(self, text_inputs, label=None):
         if label is not None:
@@ -36,7 +43,10 @@ class TextNetwork(nn.Module):
         # Perform inference to get hidden states
         with torch.no_grad():
             outputs = self.model(**text_inputs)
-            hidden_states = outputs.last_hidden_state
+            if self.encoder_type == "tapex":  # TAPEX has both encoder and decoder
+                hidden_states = outputs.encoder_last_hidden_state
+            else:
+                hidden_states = outputs.last_hidden_state
 
         # Aggregate the embeddings (e.g., by taking the mean across the token dimension)
         row_embeddings = hidden_states.mean(dim=1)  # Mean of hidden states
