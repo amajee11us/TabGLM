0ba45c93c5b08a386d11a330682fffc4da0464d0
Anay Majee <anaymajee11us@gmail.com>
2024-07-17 19:03:57 -0700

    Added new numerical+categorical datasets to TGRL

---
+++ tgrl/data/datasets.py
@@ -0,0 +1,253 @@
+'''
+This is a placeholder for all tabular data loaders
+'''
+# from tgrl.data.utils import byte_to_string_columns
+import os
+import openml
+import yaml
+import numpy as np
+import pandas as pd
+from scipy.io.arff import loadarff
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+DATASET_ROOT = "datasets"
+
+binary_mapping = {True: 1, False: 0}
+
+'''
+Same OpenML dataset can have multiple IDs.
+Thus, we map dataset names to correct IDs.
+'''
+openml_dataset_name_to_id = {
+    "pc3": 1050,
+    "kr_vs_kp": 3,
+    "mfeat_fourier": 971,
+    "coil2000": 298,
+    "texture": 40499
+}
+
+def byte_to_string_columns(data):
+    # Convert Column Names in bytes to string
+    for col, dtype in data.dtypes.items():
+        if dtype == object:  # Only process byte object columns.
+            data[col] = data[col].apply(lambda x: x.decode("utf-8"))
+    return data
+
+def get_openml_dataset(dataset_name, target, random_state):
+    # Get dataset by name
+    dataset = openml.datasets.get_dataset(
+        openml_dataset_name_to_id[dataset_name]
+    )
+    print(dataset)
+
+    # Get the data itself as a dataframe (or otherwise)
+    df, y, _, _ = dataset.get_data(dataset_format="dataframe")
+
+    for column in df.columns:
+        if df[column].dtype == "category":  # Assuming object dtype implies categorical
+            df[column] = pd.factorize(df[column])[0]   
+    
+    X, y = df.drop(target, axis=1), df[target]
+    X = X.loc[:, X.nunique() > 1]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=random_state
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=random_state
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def get_blood_dataset(dataset_name, target, random_state):
+    columns = {'V1': 'recency', 'V2': 'frequency', 'V3': 'monetary', 'V4': 'time', 'Class': 'label'}
+    dataset = pd.DataFrame(loadarff(os.path.join(
+        DATASET_ROOT, dataset_name, 'php0iVrYT.arff'))[0])
+    dataset = byte_to_string_columns(dataset)
+    dataset.rename(columns=columns, inplace=True)
+    dataset['label'] = dataset['label'] == '2'
+    dataset['label'] = dataset['label'].replace(binary_mapping)
+
+    X, y = dataset.drop(target, axis=1), dataset[target]
+    X = X.loc[:, X.nunique() > 1]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=random_state
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=random_state
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def get_bank_dataset(dataset_name, target, random_state):
+    columns = ['age', 'job', 'marital', 'education', 'default', 'balance', 'housing', 'loan', 'contact', 'day',
+                   'month', 'duration', 'campaign', 'pdays', 'previous', 'poutcome']
+    columns = {'V' + str(i + 1): v for i, v in enumerate(columns)}
+    dataset = pd.DataFrame(loadarff(
+        os.path.join(DATASET_ROOT, dataset_name, 'phpkIxskf.arff'))[0])
+    dataset = byte_to_string_columns(dataset)
+    dataset.rename(columns=columns, inplace=True)
+    dataset.rename(columns={'Class': 'label'}, inplace=True)
+    dataset['label'] = dataset['label'] == '2'
+    dataset['label'] = dataset['label'].replace(binary_mapping)
+
+    X, y = dataset.drop(target, axis=1), dataset[target]
+    X = X.loc[:, X.nunique() > 1]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=random_state
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=random_state
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def get_calhousing_dataset(dataset_name, target, random_state):
+    dataset = pd.DataFrame(loadarff(os.path.join(DATASET_ROOT, dataset_name, 'houses.arff'))[0])
+    dataset = byte_to_string_columns(dataset)
+    dataset.rename(columns={'median_house_value': 'label'}, inplace=True)
+    # Make binary task by labelling upper half as true
+    median_price = dataset['label'].median()
+    dataset['label'] = dataset['label'] > median_price
+    dataset['label'] = dataset['label'].replace(binary_mapping) 
+
+    X, y = dataset.drop(target, axis=1), dataset[target]
+    X = X.loc[:, X.nunique() > 1]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=random_state
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=random_state
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def get_car_dataset(dataset_name, target, random_state):
+    columns = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety_dict', 'label']
+    dataset = pd.read_csv(os.path.join(DATASET_ROOT, dataset_name, 'car.data'), names=columns)
+    original_size = len(dataset)
+    label_dict = {'unacc': 0, 'acc': 1, 'good': 2, 'vgood': 3}
+    dataset['label'] = dataset['label'].replace(label_dict)
+
+    X, y = dataset.drop(target, axis=1), dataset[target]
+    X = X.loc[:, X.nunique() > 1]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=random_state
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=random_state
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def get_diabetes_dataset(dataset_name, target, random_state):
+    dataset = pd.read_csv(os.path.join(DATASET_ROOT, dataset_name, 'diabetes.csv'))
+    original_size = len(dataset)
+    dataset = dataset.rename(columns={'Outcome': 'label'})
+
+    X, y = dataset.drop(target, axis=1), dataset[target]
+    X = X.loc[:, X.nunique() > 1]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=random_state
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=random_state
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def get_creditg_dataset(dataset_name, target, random_state):
+    dataset = pd.DataFrame(loadarff(os.path.join(DATASET_ROOT, dataset_name, 'dataset_31_credit-g.arff'))[0])
+    dataset = byte_to_string_columns(dataset)
+    dataset.rename(columns={'class': 'label'}, inplace=True)
+    dataset['label'] = dataset['label'] == 'good'
+    dataset['label'] = dataset['label'].replace(binary_mapping)
+
+    X, y = dataset.drop(target, axis=1), dataset[target]
+    X = X.loc[:, X.nunique() > 1]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=random_state
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=random_state
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test    
+
+def get_heart_dataset(dataset_name, target, random_state):
+    dataset = pd.read_csv(os.path.join(DATASET_ROOT, dataset_name, 'heart.csv'))
+    original_size = len(dataset)
+    dataset = dataset.rename(columns={'HeartDisease': 'label'})
+
+    X, y = dataset.drop(target, axis=1), dataset[target]
+    X = X.loc[:, X.nunique() > 1]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=random_state
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=random_state
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def get_jungle_dataset(dataset_name, target, random_state):
+    dataset = pd.DataFrame(loadarff(os.path.join(DATASET_ROOT, dataset_name, 'jungle_chess_2pcs_raw_endgame_complete.arff'))[0])
+    dataset = byte_to_string_columns(dataset)
+    dataset.rename(columns={'class': 'label'}, inplace=True)
+    dataset['label'] = dataset['label'] == 'w'  # Does white win?
+    dataset['label'] = dataset['label'].replace(binary_mapping)
+
+    X, y = dataset.drop(target, axis=1), dataset[target]
+    X = X.loc[:, X.nunique() > 1]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=random_state
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=random_state
+    )
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def get_income_dataset(dataset_name, target, random_state):    
+    columns = ['age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status', 'occupation',
+                'relationship', 'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week',
+                'native_country', 'label']
+
+    def strip_string_columns(df):
+        df[df.select_dtypes(['object']).columns] = df.select_dtypes(['object']).apply(lambda x: x.str.strip())
+
+    dataset_train = pd.read_csv(os.path.join(DATASET_ROOT, dataset_name, 'adult.data'), names=columns, na_values=['?', ' ?'])
+    dataset_train = dataset_train.drop(columns=['fnlwgt', 'education_num'])
+    original_size = len(dataset_train)
+    strip_string_columns(dataset_train)
+    dataset_train['label'] = dataset_train['label'] == '>50K'
+    dataset['label'] = dataset['label'].replace(binary_mapping)
+
+    X, y = dataset_train.drop(target, axis=1), dataset_train[target]
+    X = X.loc[:, X.nunique() > 1]
+    X_train, X_val, y_train, y_val = train_test_split(X, y, 
+                                                    test_size=0.2, 
+                                                    random_state=random_state)
+    dataset = dataset_train
+    assert len(y_train) + len(y_val) == original_size
+
+    dataset_test = pd.read_csv(os.path.join(DATASET_ROOT, dataset_name, 'adult.test'), names=columns, na_values=['?', ' ?'])
+    dataset_test = dataset_test.drop(columns=['fnlwgt', 'education_num'])
+    strip_string_columns(dataset_test)
+    dataset_test['label'] = dataset_test['label'] == '>50K.'
+    dataset['label'] = dataset['label'].replace(binary_mapping)
+
+    X_test, y_test = dataset_test.drop(target, axis=1), dataset_test[target]
+    X_test = X_test.loc[:, X_test.nunique() > 1]
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
--- tgrl/data/utils.py
+++ tgrl/data/utils.py
@@ -5,39 +5,30 @@ import numpy as np
 import pandas as pd
 from typing import Optional
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, StandardScaler, LabelEncoder
 from torch.utils.data import DataLoader
 
 from tgrl.utils import set_seed
+from tgrl.data.datasets import *
 
-
-def pick_dataset_openml(dataset_name, target, random_state):
-    """
-    Fetch OpenML dataset - General wrapper for dataset collections
-    """
-    # Get dataset by name
-    dataset = openml.datasets.get_dataset(dataset_name)
-    print(dataset)
-
-    # Get the data itself as a dataframe (or otherwise)
-    df, y, _, _ = dataset.get_data(dataset_format="dataframe")
-
-    for column in df.columns:
-        if df[column].dtype == "category":  # Assuming object dtype implies categorical
-            df[column] = pd.factorize(df[column])[0]
-
-    X, y = df.drop(target, axis=1), df[target]
-    X = X.loc[:, X.nunique() > 1]
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.25, random_state=random_state
-    )
-    X_train, X_val, y_train, y_val = train_test_split(
-        X_train, y_train, test_size=0.2, random_state=random_state
-    )
-
-    return X_train, X_val, X_test, y_train, y_val, y_test
-
+dataset_factory = {
+    # TabLLM benchmark datasets
+    "bank"         : get_bank_dataset,
+    "blood"        : get_blood_dataset,
+    "calhousing"   : get_calhousing_dataset,
+    "car"          : get_car_dataset,
+    "creditg"      : get_creditg_dataset,
+    "diabetes"     : get_diabetes_dataset,
+    "heart"        : get_heart_dataset,
+    "income"       : get_income_dataset,
+    "jungle"       : get_jungle_dataset,
+    # OpenML benchmark datasets
+    "pc3"          : get_openml_dataset,
+    "kr_vs_kp"     : get_openml_dataset,
+    "mfeat_fourier": get_openml_dataset,
+    "coil2000"     : get_openml_dataset,
+    "texture"      : get_openml_dataset
+}
 
 def to_dense_array(series):
     # Check if the series is stored as a sparse array
@@ -92,45 +83,102 @@ def preprocess_and_load_data(data_config=None):
     set_seed(data_config["random_state"])
 
     # Load the dataset
-    X_train, X_val, X_test, y_train, y_val, y_test = pick_dataset_openml(
+    assert config["dataset"] in  list(dataset_factory.keys())
+
+    X_train, X_val, X_test, y_train, y_val, y_test = dataset_factory[config["dataset"]](
         config["dataset"], config["target"], config["random_state"]
     )
 
-    # Initialize the scalers
+    # # Initialize the scalers
     ln = Norm2Scaler()
     scaler_y = (
         StandardScaler() if config["task_type"] == "regression" else LabelEncoder()
     )
 
-    # Normalize the features
-    X_train_norm = pd.DataFrame(
-        ln.fit_transform(X_train.to_numpy()), columns=X_train.columns
-    )
-    X_val_norm = pd.DataFrame(ln.transform(X_val.to_numpy()), columns=X_val.columns)
-    X_test_norm = pd.DataFrame(ln.transform(X_test.to_numpy()), columns=X_test.columns)
+    # # Normalize the features
+    # X_train_norm = pd.DataFrame(
+    #     ln.fit_transform(X_train.to_numpy()), columns=X_train.columns
+    # )
+    # X_val_norm = pd.DataFrame(ln.transform(X_val.to_numpy()), columns=X_val.columns)
+    # X_test_norm = pd.DataFrame(ln.transform(X_test.to_numpy()), columns=X_test.columns)
 
-    # Calculate the percentage of NAs in each column
-    percent_nas = X_train_norm.isna().mean()
-    # Create a mask for columns with less than 15% NAs
-    columns_to_keep = percent_nas < config["na_threshold"]
+    categorical_columns = X_train.select_dtypes(include=['object', 'category']).columns.tolist()
+    numerical_columns = X_train.select_dtypes(include=['number']).columns.tolist()
 
-    # Use the mask to select columns with less than 15% NAs in all sets
-    X_train_norm = X_train_norm.loc[:, columns_to_keep]
-    X_val_norm = X_val_norm.loc[:, columns_to_keep]
-    X_test_norm = X_test_norm.loc[:, columns_to_keep]
+    print(f"Number of Categorical columns: {len(categorical_columns)}")
+    print(f"Number of Numerical columns: {len(numerical_columns)}")
 
-    # Remove rows with any NAs from X and corresponding rows from y
-    rows_to_keep_train = ~X_train_norm.isna().any(axis=1)
-    X_train_norm = X_train_norm.loc[rows_to_keep_train]
-    y_train = y_train[rows_to_keep_train.to_numpy()]  # Apply the same mask to y_train
 
-    rows_to_keep_val = ~X_val_norm.isna().any(axis=1)
-    X_val_norm = X_val_norm.loc[rows_to_keep_val]
-    y_val = y_val[rows_to_keep_val.to_numpy()]  # Apply the same mask to y_val
+    # Remove any NaN values for numerical data replace with mean 
+    X_train[numerical_columns] = X_train[numerical_columns].apply(lambda x: x.fillna(x.mean()), axis=0)
+    X_val[numerical_columns] = X_val[numerical_columns].apply(lambda x: x.fillna(x.mean()), axis=0)
+    X_test[numerical_columns] = X_test[numerical_columns].apply(lambda x: x.fillna(x.mean()), axis=0)
+    
+    # Function to drop rows with NaNs in categorical columns in both X and y
+    def drop_na_categorical(X, y, categorical_columns):
+        na_indices = X[categorical_columns].isna().any(axis=1)
+        X_clean = X[~na_indices]
+        y_clean = y[~na_indices]
+        return X_clean, y_clean
 
-    rows_to_keep_test = ~X_test_norm.isna().any(axis=1)
-    X_test_norm = X_test_norm.loc[rows_to_keep_test]
-    y_test = y_test[rows_to_keep_test.to_numpy()]  # Apply the same mask to y_test
+    # Drop rows with NaNs in categorical columns for train, validation, and test sets
+    X_train, y_train = drop_na_categorical(X_train, y_train, categorical_columns)
+    X_val, y_val = drop_na_categorical(X_val, y_val, categorical_columns)
+    X_test, y_test = drop_na_categorical(X_test, y_test, categorical_columns)
+
+    # Extract the numerical columns and normalize them using only X_train
+    scaler = MinMaxScaler()
+    normalized_numerical_X_train = scaler.fit_transform(X_train[numerical_columns])
+    normalized_numerical_X_val = scaler.transform(X_val[numerical_columns])
+    normalized_numerical_X_test = scaler.transform(X_test[numerical_columns])
+
+    normalized_numerical_df_train = pd.DataFrame(
+        normalized_numerical_X_train, columns=numerical_columns)
+    normalized_numerical_df_val = pd.DataFrame(
+        normalized_numerical_X_val, columns=numerical_columns)
+    normalized_numerical_df_test = pd.DataFrame(
+        normalized_numerical_X_test, columns=numerical_columns)
+
+    # Extract the categorical columns and perform one-hot encoding using only X_train
+    encoder = OneHotEncoder(sparse_output=False)
+    encoded_categorical_X_train = encoder.fit_transform(X_train[categorical_columns])
+    encoded_categorical_X_val = encoder.transform(X_val[categorical_columns])
+    encoded_categorical_X_test = encoder.transform(X_test[categorical_columns])
+
+    encoded_categorical_df_train = pd.DataFrame(
+        encoded_categorical_X_train, columns=encoder.get_feature_names_out(categorical_columns))
+    encoded_categorical_df_val = pd.DataFrame(
+        encoded_categorical_X_val, columns=encoder.get_feature_names_out(categorical_columns))
+    encoded_categorical_df_test = pd.DataFrame(
+        encoded_categorical_X_test, columns=encoder.get_feature_names_out(categorical_columns))
+
+    # Concatenate the normalized numerical data and the encoded categorical data for X_train, X_val, and X_test
+    X_train_norm = pd.concat([normalized_numerical_df_train, encoded_categorical_df_train], axis=1)
+    X_val_norm = pd.concat([normalized_numerical_df_val, encoded_categorical_df_val], axis=1)
+    X_test_norm = pd.concat([normalized_numerical_df_test, encoded_categorical_df_test], axis=1)
+
+    # # Calculate the percentage of NAs in each column
+    # percent_nas = X_train_norm.isna().mean()
+    # # Create a mask for columns with less than 15% NAs
+    # columns_to_keep = percent_nas < config["na_threshold"]
+
+    # # Use the mask to select columns with less than 15% NAs in all sets
+    # X_train_norm = X_train_norm.loc[:, columns_to_keep]
+    # X_val_norm = X_val_norm.loc[:, columns_to_keep]
+    # X_test_norm = X_test_norm.loc[:, columns_to_keep]
+
+    # # Remove rows with any NAs from X and corresponding rows from y
+    # rows_to_keep_train = ~X_train_norm.isna().any(axis=1)
+    # X_train_norm = X_train_norm.loc[rows_to_keep_train]
+    # y_train = y_train[rows_to_keep_train.to_numpy()]  # Apply the same mask to y_train
+
+    # rows_to_keep_val = ~X_val_norm.isna().any(axis=1)
+    # X_val_norm = X_val_norm.loc[rows_to_keep_val]
+    # y_val = y_val[rows_to_keep_val.to_numpy()]  # Apply the same mask to y_val
+
+    # rows_to_keep_test = ~X_test_norm.isna().any(axis=1)
+    # X_test_norm = X_test_norm.loc[rows_to_keep_test]
+    # y_test = y_test[rows_to_keep_test.to_numpy()]  # Apply the same mask to y_test
 
     # Normalize/Encode the target variable
     if config["task_type"] == "regression":
